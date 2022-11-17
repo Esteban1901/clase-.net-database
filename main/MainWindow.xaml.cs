df931318f5036659380d719f69b3d28477d3a1a6
AlumnosInf <AlumnosInf@soportetiu.local>
2022-11-17 16:00:19 -0300

    Aquí van los avances de la primera clase

---
+++ main/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BLL;
+namespace main
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClasificacionBLL cbll = new ClasificacionBLL();
+        TareasBLL tbll = new TareasBLL();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListClasificacion.ItemsSource = cbll.GetAll();
+            ListTarea.ItemsSource = tbll.GetAll();
+            
+        }
+
+        private void BtnNuevaClasificacion_Click(object sender, RoutedEventArgs e)
+        {
+            string nombre = txtClasificacion.Text;
+
+            //Validaciones de entrada 
+
+            cbll.Add(nombre);
+
+            MessageBox.Show("Clasificación creada", " Nueva clasificación", MessageBoxButton.OK, MessageBoxImage.Information);
+            txtClasificacion.Text = string.Empty;
+
+            ListClasificacion.ItemsSource = null;
+            ListClasificacion.ItemsSource = cbll.GetAll();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string Titulo = TxtTitulo.Text;
+            string Detalle = TxtDetalle.Text;
+            DateTime FechaVenc = (DateTime)Fecha.SelectedDate;
+
+            tbll.Add(Titulo, Detalle, FechaVenc);
+
+            MessageBox.Show("Tarea creada", " Nueva tarea", MessageBoxButton.OK, MessageBoxImage.Information);
+            TxtTitulo.Text = string.Empty;
+            TxtDetalle.Text = string.Empty;
+
+
+            ListTarea.ItemsSource = null;
+            ListTarea.ItemsSource = tbll.GetAll();
+        }
+    }
+}
---
+++ negocio/ClasificacionBLL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using datos;
+namespace BLL
+{
+
+    public class TareasBLL {
+        TareasPendientesEntities Bd = new TareasPendientesEntities();
+
+        public void Add(string Titulo, string Detalle, DateTime FechaVencimiento)
+        {
+            Tarea nuevas = new Tarea();
+            nuevas.Titulo = Titulo;
+            nuevas.Detalle = Detalle;
+            nuevas.FechaVencimiento = FechaVencimiento;
+
+            Bd.Tarea.Add(nuevas);
+            Bd.SaveChanges();
+        }
+        public List<Tarea> GetAll()
+        {
+            return Bd.Tarea.ToList();
+        }
+    }
+    public class ClasificacionBLL
+    {
+        TareasPendientesEntities bd = new TareasPendientesEntities();
+
+        public void Add(string nombre)
+        {
+            Clasificacion nueva = new Clasificacion();
+            nueva.Nombre = nombre;
+
+            bd.Clasificacion.Add(nueva);
+            bd.SaveChanges();
+        }
+
+        public List<Clasificacion> GetAll()
+        {
+            return bd.Clasificacion.ToList();
+        }
+    }
+}
